1297be74bac6445b683ebe7051948fdee4714769
eduardo <l.eduardos.lopes@gmail.com>
2022-01-25 18:37:25 -0200

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,24 @@ const db = nanodb.use('equipamentos');
 const express = require('express')
 const router = express.Router()
 
-
+//visualização
 router.get('/:id',async (req,res) => {
   const doc = await db.get(req.params.id)
   res.statusCode(200).send(`${doc.descricao}`)
 })
+//criação
+router.post('/add', async (req,res) => {
 
+})
 
+//edição
+router.put('/:id/edit', async (req,res) => {
+
+})
+
+//delete
+router.delete('/:id/delete', () => {
+
+})
 
 module.exports = router

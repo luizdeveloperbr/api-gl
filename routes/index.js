c2a686bfbe85b9e8ebe8e9b9368c5f367c5c1724
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2022-01-25 23:48:41 -0300

    função de  visualização e inserção de dados

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,38 @@ const express = require('express')
 const router = express.Router()
 
 //visualização
-router.get('/:id',async (req,res) => {
-  const doc = await db.get(req.params.id)
-  res.statusCode(200).send(`${doc.descricao}`)
-})
-//criação
-router.post('/add', async (req,res) => {
 
+  router.get('/:id', (req,res) => {
+       db.get(req.params.id, (error,body,headers) => {
+         if(error){return res.send(error.message)}
+         res.send(body)
+       })
+
+  })
+
+//criação
+router.post('/add', async (req,res) => { 
+    db.insert({_id:req.query.id,descricao:req.query.desc}, (error,body,headers) => {
+      if(error){return res.send(error.message)}
+      res.send('dados inseridos com sucesso')
+    })
 })
 
 //edição
-router.put('/:id/edit', async (req,res) => {
-
+router.put('/:id/edit', (req,res) => {
+  var doc
+  db.get(req.params.id, (error,body,headers) => {
+    if(error){return res.send(error.message)}
+    doc = body
+    console.log(doc)
+  })
+  db.insert({...doc,...req.query})
+  res.send('updated')
 })
-
+/*
 //delete
 router.delete('/:id/delete', () => {
 
 })
-
+*/
 module.exports = router

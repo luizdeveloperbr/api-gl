92138d3c73a62bd5c60afdae29a845cf21d023b0
luizdeveloperbr <l.eduardos.lopes@gmail.com>
2022-02-07 23:14:29 -0300

    docker working

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM node:lts-alpine
 WORKDIR /usr/src/app
-COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+COPY package*.json /usr/src/app/
 RUN npm install
 COPY . .
-EXPOSE 3000
+EXPOSE 5000
 RUN chown -R node /usr/src/app
 USER node
 CMD ["npm", "start"]
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@
 const dotenv = require('dotenv');dotenv.config()
 const nano = require('nano')(`http://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}:${process.env.DB_PORT}`);
 
-const openDb = async () => {
-  const dbName = await nano.db.list()
-  if (!dbName.includes(process.env.DB_NAME)) {
-   nano.db.create(process.env.DB_NAME)
-  }
-  return
+const openDb = async (createDbName) => {
+	const dbName = await nano.db.list()
+	if (!dbName.includes(createDbName)) {
+	 nano.db.create(createDbName)
+	 console.log('Database criado com o nome: '+createDbName)
+	 return
+	}else{
+	console.log('Database já existe')
+	return
+	}
 }
+openDb(process.env.DB_NAME)
 const db = nano.use(process.env.DB_NAME)
 
 //Express.js para configuração das rotas
@@ -32,6 +37,7 @@ router.post('/add',(req,res) => {
          res.status(200).send(body)
        })
   })
+
 
 //edição
 router.put('/:id/edit', (req,res) => {
